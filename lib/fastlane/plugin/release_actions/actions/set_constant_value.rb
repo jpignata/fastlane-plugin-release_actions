12c0eaea5899a771131965ce65519e4fd6baa64e
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2020-06-23 11:55:32 -0400

    use variable instead of constant. fix lint errors

--- lib/fastlane/plugin/release_actions/actions/set_constant_value.rb
+++ lib/fastlane/plugin/release_actions/actions/set_constant_value.rb
@@ -5,21 +5,20 @@ module Fastlane
         file = params[:file]
         constant = params[:constant]
         value = params[:value]
-        
+
+        regex_constant_version = /(?<=#{constant})(\s*=\s*["'].*)/
         file_contents = File.read(file)
 
-        CONSTANT_VERSION = /(?<=#{constant})(\s*=\s*["'].*)/
-  
-        if !file_contents.match(CONSTANT_VERSION)
+        unless file_contents.match(regex_constant_version)
           UI.error("#{constant} not present or doesn't have an explicit value in #{file}")
           return
         end
 
         new_value = " = '#{value}'"
-        
-        file_contents = file_contents.gsub(CONSTANT_VERSION, new_value)
 
-        File.open(file, "w") { |file| file.puts file_contents }
+        file_contents = file_contents.gsub(regex_constant_version, new_value)
+
+        File.open(file, "w") { |f| f.puts(file_contents) }
         UI.success("Successfully modified #{constant} to value #{value} in #{file}")
       end
 
@@ -33,27 +32,27 @@ module Fastlane
 
       def self.available_options
         [
-            FastlaneCore::ConfigItem.new(
-                key: :file,
-                env_name: 'FILE_PATH',
-                description: 'The path of the file you wish to modify',
-                optional: false,
-                type: String
-            ),
-            FastlaneCore::ConfigItem.new(
-                key: :constant,
-                env_name: 'CONSTANT_NAME',
-                description: 'The constant you wish to modify',
-                optional: false,
-                type: String
-            ),
-            FastlaneCore::ConfigItem.new(
-                key: :value,
-                env_name: 'CONSTANT_VALUE',
-                description: 'The new value to assign to the constant',
-                optional: false,
-                type: String
-            )
+          FastlaneCore::ConfigItem.new(
+            key: :file,
+            env_name: 'FILE_PATH',
+            description: 'The path of the file you wish to modify',
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :constant,
+            env_name: 'CONSTANT_NAME',
+            description: 'The constant you wish to modify',
+            optional: false,
+            type: String
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :value,
+            env_name: 'CONSTANT_VALUE',
+            description: 'The new value to assign to the constant',
+            optional: false,
+            type: String
+          )
         ]
       end
 
